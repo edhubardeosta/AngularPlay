96e93413f2925d747f423dba9e68d30d8a1d35dc
edhubardeosta <ausweichen@outlook.com>
2024-05-01 14:12:39 +0200

    Finished splitting loadscreen into custom component.

--- Eduardeo-App/src/app/app.component.ts
+++ Eduardeo-App/src/app/app.component.ts
@@ -3,9 +3,8 @@ import {MatProgressBar, MatProgressBarModule} from '@angular/material/progress-b
 import {MatButtonModule, MatFabButton, MatButton} from '@angular/material/button'; 
 import {MatCardModule} from '@angular/material/card'; 
 
-var progressBarValueInternal:number = 0;
+var progressBarValueInternal:number;
 var loadScreenClassesInternal:Array<string> = ["hidden"];
-var intervId:any;
 
 @Component({
   selector: 'app-root',
@@ -21,34 +20,12 @@ export class AppComponent {
   loadScreenClasses = loadScreenClassesInternal;
   start($event:any){
     this.loadScreenHidden = false; 
-    this.progressBarValue = 30;
-    //fakeProgressBar(this, intervId, main, [this])
+    this.progressBarValue = 100;
   }
-}
-function hide(element:any){
-  
-}
-function docReady(fn:any) {
-  // see if DOM is already available
-  if (document.readyState === "complete" || document.readyState === "interactive") {
-      // call on next available tick
-      setTimeout(fn, 1);
-  } else {
-      document.addEventListener("DOMContentLoaded", fn);
+  loaded($event:any){
+    this.loadScreenHidden = true; 
+    this.gameHidden = false;
   }
-}    
-function fakeProgressBar(context:any, intervId:any, callback: Function, callbackArgs: Array<any>) {
-  console.log("fakeProgressBar started.");
-  if (!intervId) {
-    intervId = setInterval(()=>{console.log("progressBarValue:", context.progressBarValue); context.progressBarValue += 1; if(context.progressBarValue>99){stopProgressBar(intervId,callback, callbackArgs)}}, 50);
-  }
-}
-function stopProgressBar(intervId:any, callback: Function, args: Array<any>) {
-  console.log("stopProgressBar started. Args:", args);
-  clearInterval(intervId);
-  // release our intervalID from the variable
-  intervId = null;
-  callback.apply(null, args);
 }
 function main(context:any):void{
   console.log("Main has started! Context: ", context);
@@ -63,8 +40,3 @@ function test2():void{
   console.log("setting progress bar value to 25");
   progressBarValueInternal=25;
 }
-//setTimeout(()=>{console.log("Setting ProgressBar to 75"); progressBarValueInternal=75;}, 5000);
-
-//main();
-//console.log("Script started.");
-//console.log("Progress Bar Value:", progressBarValueInternal);
--- Eduardeo-App/src/app/loadingscreen/loadingscreen.component.ts
+++ Eduardeo-App/src/app/loadingscreen/loadingscreen.component.ts
@@ -1,12 +1,13 @@
-import { Component, ViewChild, Output, Input, EventEmitter } from '@angular/core';
+import { Component, ViewChild, Output, Input, EventEmitter, OnChanges } from '@angular/core';
 import {MatProgressBar, MatProgressBarModule} from '@angular/material/progress-bar'; 
 
+var loadIntervId:any;
 @Component({
   selector: 'app-loadingscreen',
   templateUrl: './loadingscreen.component.html',
   styleUrl: './loadingscreen.component.css'
 })
-export class LoadingscreenComponent {
+export class LoadingscreenComponent implements OnChanges {
 progressBarValue = 0;
   @Input() progressBarValueInternal = 0;
   @Input() loadScreenHidden = true;
@@ -15,8 +16,25 @@ progressBarValue = 0;
 
   @ViewChild('loadingProgressBar', {static: false}) loadingProgressBar!: MatProgressBar;
 
-  ngOnchanges(changes:any){
+  ngOnChanges(changes:any){
     console.log("Change on Loadingscreen detected:", changes);
+    if(changes.progressBarValueInternal?.currentValue != changes.progressBarValueInternal?.previousValue){
+      console.log("Change on progressBarValueInternal detected!");
+      loadUpTo(this.progressBarValueInternal, this, loadIntervId, ()=>{this.finishedLoading.emit(this.progressBarValueInternal);}, []);
+    }
   }
 
+}
+function loadUpTo(targetValue:number, context:any, intervId:any, callback: Function, callbackArgs: Array<any>) {
+  console.log("fakeProgressBar started.");
+  if (!intervId) {
+    intervId = setInterval(()=>{console.log("progressBarValue:", context.progressBarValue); context.progressBarValue += 1; if(context.progressBarValue>targetValue-1){stopProgressBar(intervId,callback, callbackArgs)}}, 50);
+  }
+}
+function stopProgressBar(intervId:any, callback: Function, args: Array<any> = []){
+  console.log("stopProgressBar started. Args:", args);
+  clearInterval(intervId);
+  // release our intervalID from the variable
+  intervId = null;
+  callback.apply(null, args);
 }
